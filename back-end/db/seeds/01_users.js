1391592c54666fe5519f214d9236dceda84a04f9
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-05-25 13:10:02 +0900

    fix: fix 2 for bad gateway

--- back-end/db/seeds/01_users.js
+++ back-end/db/seeds/01_users.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
+  await knex("cases").del();
   await knex("users").del();
   await knex("users").insert([
     {
